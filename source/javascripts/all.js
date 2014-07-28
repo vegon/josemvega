593be126a3534da9c5bf4c8abec2c5a86cb3e876
Vega <jm.vega.farina@gmail.com>
2014-07-28 12:44:01 +0200

    cambio a middlemanapp

---
+++ source/javascripts/all.js
@@ -0,0 +1,8 @@
+//= require vendor/jquery.min.js
+//= require vendor/foundation.min.js
+//= require vendor/TweenMax.min.js
+//= require vendor/jquery.scrollmagic.min.js
+//= require vendor/waypoints.min.js
+//= require vendor/jquery.nav.js
+//= require vendor/jquery.unveil.min.js
+//= require custom.js
